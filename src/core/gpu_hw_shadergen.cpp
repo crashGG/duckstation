34a030765b661258f7d12c5f44a8d06c4b3c1c14
crashGG <80488411+crashGG@users.noreply.github.com>
2025-08-30 00:41:11 +0800

    test ialpha

--- src/core/gpu_hw_shadergen.cpp
+++ src/core/gpu_hw_shadergen.cpp
@@ -1286,7 +1286,8 @@ void FilteredSampleFromVRAM(TEXPAGE_VALUE texpage, float2 coords, float4 uv_limi
   float2 fpart = frac(coords);
   float4 res = (fpart.x < 0.5f) ? ((fpart.y < 0.5f) ? J : L) : ((fpart.y < 0.5f) ? K : M);
 
-  ialpha = float(res.a > 0.0001f);
+  //ialpha = float(res.a > 0.0001f);
+  ialpha = 1.0f;
   texcol = res;
   }
 
